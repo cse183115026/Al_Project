a1e53ed95edbcc456b431e72fbfd760b22150dba
sujon biswas <66966376+cse183115026@users.noreply.github.com>
2021-06-19 23:03:51 +0600

    AI_2nd update

---
+++ SpeeechRecognition/main.js
@@ -0,0 +1,33 @@
+const texts= document.querySelector('.texts');
+window.SpeechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition; 
+
+const recognition =new window.SpeechRecognition();
+recognition.interimResults= true;
+
+let p= document.createElement("p");
+
+recognition.addEventListener('result',(e) => {
+
+    const text = Array.from(e.results)
+    .map(results => results[0])
+    .map(results => results.transcript)
+    .join('');
+
+    p.innerText =text;
+    texts.appendChild(p);
+    if(e.results[0].isFinal){
+        if(text.includes('hello')){
+            p=document.createElement('p');
+            p.classList.add('replay');
+            p.innerText='Hi';
+            texts.appendChild(p);
+        }
+    }
+   
+    console.log(e);
+})
+
+recognition.addEventListener('end', ()=>{
+    recognition.start();
+})
+
